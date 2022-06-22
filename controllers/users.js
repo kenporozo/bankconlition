254cec85dd0e164b5868869988cf70cbd44e31de
Jona <kenporozo12@gmail.com>
2022-06-21 20:28:52 -0400

    singleton added in the connection to db + refactor in controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,33 +1,10 @@
 const { response, request } = require("express");
 const bcryptjs = require("bcryptjs");
-const db = require("../database/config");
+const db = require("../database/connection");
 
 const getUsers = (req = request, res = response) =>{
-    db.getConnection((err, connection) => {
-        if (err) {
-          console.log(`Connection error: ${err.sqlMessage}`);
-          connection.release();
-          return res.json({
-            codRsp : 1,
-            msgRsp : `There's was an error in the connection to the DB: ${err.sqlMessage}`
-          });
-        }
-        
-        connection.query("CALL sp_sel_usuario()", (err, rows)=>{
-            if(err){
-                console.log(`Error executing the SP: ${err.sqlMessage}`);
-                return res.json({
-                    codRsp : 1,
-                    msgRsp : `There's was an error executing the SP: ${err.sqlMessage}`
-                  });
-            };
-            res.json({
-               users : rows[0]
-              });
-            console.log(`Response = ${JSON.stringify(rows[0])}`);
-        });
-        connection.release();
-      });
+      let sql = "CALL sp_sel_usuario()";
+      db(sql, res, (result) => res.json({users : result}));
       /**
        * #swagger.tags = ["Users"]
        * #swagger.description = "Endpoint para obtener una lista con todos los usuarios del sistema"
---
+++ database/connection.js
@@ -0,0 +1,63 @@
+const mysql = require("mysql");
+let pool;
+const ERROR_CODE = 500;
+
+const getPool = () => {
+  if (pool) {
+    console.log("Ya hay una conexión, esto es singleton :)");
+    return pool;
+  }
+
+  pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    database: process.env.DB_SCHEMA,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  });
+  return pool;
+};
+
+const errorResponse = (error, response) => {
+  console.log(`--ERROR| ${error} |FIN ERROR--`);
+  response.status(ERROR_CODE).send({
+    codRsp: ERROR_CODE,
+    msgRsp: error,
+  });
+};
+
+const excuteSP = (sql, response, callback) => {
+  try {
+    getPool().getConnection((err, connection) => {
+      if (err) {
+        try {
+          console.log(`Connection error: ${err.sqlMessage}`);
+          connection.release();
+          return errorResponse(err.sqlMessage, response);
+        } catch (error) {
+          return errorResponse(err.sqlMessage, response);
+        }
+      }
+
+      connection.query(sql, (err, rows) => {
+        if (err) {
+          console.log(`Error executing the SP: ${err.sqlMessage}`);
+          try {
+            console.log(`Connection error: ${err.sqlMessage}`);
+            connection.release();
+            return errorResponse(err.sqlMessage, response);
+          } catch (error) {
+            return errorResponse(err.sqlMessage, response);
+          }
+        }
+        callback(rows[0]);
+        console.log(`Response = ${JSON.stringify(rows[0])}`);
+      });
+      connection.release();
+    });
+  } catch (error) {
+    return errorResponse(error, response);
+  }
+};
+
+module.exports = excuteSP;
